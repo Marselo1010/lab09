3f844dd13329a83d05e2c691ac8ab6d607779494
Gianluca Marsella - gianluca.marsella@studio.unibo.it <gianluca.marsella@studio.unibo.it>
2025-11-17 16:41:45 +0100

    es fatto fino al punto 3 con qualche errore

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,13 @@
 package it.unibo.mvc;
 
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +15,50 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    /*
+     * Once the `Controller` is done, implement `SimpleGUI` class in such a way that:
+
+        1. It has a main method that starts the graphical application
+        2. In its constructor, sets up the whole view
+        3. The graphical interface consists of a `JTextArea` with a button "Save" right below 
+           (see `src/test/resources/ex02.png` for the expected result). 
+        4. SUGGESTION: Use a `JPanel` with `BorderLayout`
+        5. By default, if the graphical interface is closed the program must exit 
+           (call `setDefaultCloseOperation`)
+        6. The program asks the controller to save the file if the button "Save" gets pressed.
+
+        See `src/test/resources/ex02.png` to verify the expected aspect.
+     */
+    private final static String TITLE = "My first Java graphical interface";
+    private static final int PROPORTION = 5;
+    private final JFrame frame = new JFrame(TITLE);
+
+    SimpleGUI(){
+        final JPanel canvas = new JPanel();
+        final JTextArea text = new JTextArea();
+        canvas.setLayout(new BorderLayout());
+        final JButton button = new JButton("Save");
+        canvas.add(button,BorderLayout.SOUTH);
+        canvas.add(text,BorderLayout.CENTER);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+
+    void display(){
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        //frame.pack();
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+    }
+
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,7 +1,15 @@
 package it.unibo.mvc;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JTextArea;
 
+import it.unibo.mvc.Controller;
+import it.unibo.mvc.SimpleGUI;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,6 +17,33 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
-    private final JFrame frame = new JFrame();
+    private final static String TITLE = "My first Java graphical interface";
+    private static final int PROPORTION = 5;
+    private final JFrame frame = new JFrame(TITLE);
+
+    public SimpleGUIWithFileChooser() {
+            SimpleGUI neg = new SimpleGUI();
+            JPanel canvas2 = new JPanel();
+            canvas2.setLayout(new BorderLayout());
+            final JButton button2 = new JButton("Browse...");
+            canvas2.add(button2,BorderLayout.NORTH);
+            frame.setContentPane(canvas2);
+    }
+
+    void display(){
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        //frame.pack();
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+    }
+
+    
 
 }
